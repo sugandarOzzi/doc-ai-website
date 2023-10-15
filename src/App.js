731c04d70f9e2074cd3f1e222f44800341dd96b1
sugandar <sugandar@ozzi.tech>
2023-10-15 10:11:38 +0530

    code added to make call to open ai

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+
+const apiUrl = "https://api.openai.com/v1/chat/completions";
+// Shafiq Key
+const apiKey = "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";
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${apiKey}`,
+};
+
+const HandleOpenAPICall = async (inputValue) => {
+  const da = axios
+    .post(
+      apiUrl,
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You will be provided with a piece of code, and your task is to document it clearly.",
+          },
+          {
+            role: "user",
+            content: inputValue,
+          },
+        ],
+      },
+      { headers }
+    )
+    .then((response) => {
+      console.log("Response:", response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(
+        "Error:",
+        error.response ? error.response.data : error.message
+      );
+    });
+  return da;
+}; 
+
+
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("")
+  const handleTaskChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTask = async () => {
+    const data = await HandleOpenAPICall(inputValue);
+    console.log(data.choices[0].message.content);
+    setInputValue(data.choices[0].message.content);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section>
+        <h1>Code Documentation</h1>
+        <p> We help developer to doc your code</p>
+      </section>
+      <section>
+        <h2>Code Documentation</h2>
+        <div>
+          <textarea
+            value={inputValue}
+            onChange={handleTaskChange}
+            placeholder="Enter a new task"
+          />
+          <button onClick={handleAddTask}>Add Task</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
